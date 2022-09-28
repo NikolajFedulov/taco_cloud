d0db5f71ed83b5e00c9b96843ac2e51c80c9555e
Nikolaj Fedulov <v20nickf1@gmail.com>
2022-09-28 22:04:16 +0300

    Added validation

--- src/main/java/net/nikolaj_fedulov/taco_cloud/controller/DesignTacoController.java
+++ src/main/java/net/nikolaj_fedulov/taco_cloud/controller/DesignTacoController.java
@@ -7,8 +7,10 @@ import net.nikolaj_fedulov.taco_cloud.model.TacoOrder;
 import net.nikolaj_fedulov.taco_cloud.model.Type;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,7 +57,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processTaco (Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+    public String processTaco (@Valid Taco taco, Errors errors,@ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         tacoOrder.addTaco(taco);
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
